8e1121c53d902c68d3c2ad02492a2455305471c3
KaguilarA <kaguilara@ucenfotec.ac.cr>
2017-05-26 11:31:25 -0600

    Fix

--- js/contact_me.js
+++ js/contact_me.js
@@ -10,7 +10,10 @@ jQuery(document).ready(function(){
 				var t=/^([\w-\.]+@([\w]+\.)+[\w]{2,4})?$/;if(!t.test(jQuery.trim($(this).val()))){$(this).parent().append('<span class="error-form flash animated"><i class="fa fa-exclamation-triangle"></i></span>');e=true}
 			}
 		});
-			if(!e){formInput=$(this).serialize();$.post($(this).attr("action"),formInput,function(e){$("form#formcontactus").append('<div class="col-xs-12"><div class="alert alert-success"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><strong>Your message has been sent, we will get back to you as soon as possible !</strong></div></div>')});
+			if(!e){formInput=$(this).serialize();
+				$.post($(this).attr("action"),formInput,function(e){
+					$("form#formcontactus").append('<div class="col-xs-12"><div class="alert alert-success"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><strong>Your message has been sent, we will get back to you as soon as possible !</strong></div></div>')
+				});
 				$(".requiretop").val("")}return false});
 				$("form#formcontactus input").focus(function(){
 					$("form#formcontactus .error-form").remove();
